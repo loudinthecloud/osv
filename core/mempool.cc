loudinthecloud/osv

commit 1ea5672fc0ba25eefdecdd792bbbf7ef3701de33

memory: let the debug allocator mimic the standard allocator more closely
The standard allocator returns page-aligned addresses for large allocations.
Some osv code incorrectly relies on this.

While we should fix the incorrect code, for now, adjust the debug allocator
to return aligned addresses.

The debug allocator now uses the following layout:

  [header page][guard page][user data][pattern tail][guard page]

core/mempool.cc
@@ -670,7 +670,7 @@ struct header {
     char fence[16];
     size_t size2;
 };
-static const size_t pad_before = mmu::page_size;
+static const size_t pad_before = 2 * mmu::page_size;
 static const size_t pad_after = mmu::page_size;
 
 void* malloc(size_t size)
@@ -679,15 +679,15 @@ void* malloc(size_t size)
         return std_malloc(size);
     }
 
-    auto hsize = size + sizeof(header);
-    auto asize = align_up(hsize, mmu::page_size);
+    auto asize = align_up(size, mmu::page_size);
     auto padded_size = pad_before + asize + pad_after;
     void* v = free_area.fetch_add(padded_size, std::memory_order_relaxed);
+    mmu::vpopulate(v, mmu::page_size);
+    new (v) header(size);
     v += pad_before;
     mmu::vpopulate(v, asize);
-    auto h = new (v) header(size);
-    memset(v + hsize, '$', asize - hsize);
-    return h + 1;
+    memset(v + size, '$', asize - size);
+    return v;
 }
 
 void free(void* v)
@@ -695,14 +695,14 @@ void free(void* v)
     if (v < debug_base) {
         return std_free(v);
     }
-    auto h = static_cast<header*>(v) - 1;
+    auto h = static_cast<header*>(v - pad_before);
     auto size = h->size;
-    auto hsize = size + sizeof(header);
-    auto asize = align_up(hsize, mmu::page_size);
-    char* vv = reinterpret_cast<char*>(h);
-    assert(std::all_of(vv + hsize, vv  + asize, [=](char c) { return c == '$'; }));
+    auto asize = align_up(size, mmu::page_size);
+    char* vv = reinterpret_cast<char*>(v);
+    assert(std::all_of(vv + size, vv + asize, [=](char c) { return c == '$'; }));
     h->~header();
-    mmu::vdepopulate(h, asize);
+    mmu::vdepopulate(h, mmu::page_size);
+    mmu::vdepopulate(v, asize);
 }
 
 void* realloc(void* v, size_t size)

commit 528c507acab3bdb1b3752b063b3fbba2e671dd90

mempool: Fix GPF in debug realloc()
Starting up Cassandra with debug memory allocator GPFs as follows:

  Breakpoint 1, abort () at ../../runtime.cc:85
  85	{
  (gdb) bt
  #0  abort () at ../../runtime.cc:85
  #1  0x0000000000375812 in osv::generate_signal (siginfo=..., ef=ef@entry=0xffffc0003ffe3008) at ../../libc/signal.cc:40
  #2  0x000000000037587c in osv::handle_segmentation_fault (addr=addr@entry=18446708889768681440, ef=ef@entry=0xffffc0003ffe3008)
      at ../../libc/signal.cc:55
  #3  0x00000000002fba02 in page_fault (ef=0xffffc0003ffe3008) at ../../core/mmu.cc:876
  #4  <signal handler called>
  #5  dbg::realloc (v=v@entry=0xffffe00019b3e000, size=size@entry=16) at ../../core/mempool.cc:846
  #6  0x000000000032654c in realloc (obj=0xffffe00019b3e000, size=16) at ../../core/mempool.cc:870
  #7  0x0000100000627743 in ?? ()
  #8  0x00002000001fe770 in ?? ()
  #9  0x00002000001fe780 in ?? ()
  #10 0x00002000001fe710 in ?? ()
  #11 0x00002000001fe700 in ?? ()
  #12 0xffffe000170e8000 in ?? ()
  #13 0x0000000200000001 in ?? ()
  #14 0x0000000000000020 in ?? ()
  #15 0x00002000001ffe70 in ?? ()
  #16 0xffffe000170e0004 in ?? ()
  #17 0x000000000036f361 in strcpy (dest=0x100001087420 "", src=<optimized out>) at ../../libc/string/strcpy.c:8
  #18 0x0000100000629b53 in ?? ()
  #19 0xffffe00019b22000 in ?? ()
  #20 0x0000000000000001 in ?? ()
  #21 0x0000000000000000 in ?? ()

The problem was introduced in commit 1ea5672 ("memory: let the debug
allocator mimic the standard allocator more closely") which forgot
to convert realloc() to use 'pad_before'.

core/mempool.cc
@@ -841,7 +841,7 @@ void free(void* v)
 
 void* realloc(void* v, size_t size)
 {
-    auto h = static_cast<header*>(v) - 1;
+    auto h = static_cast<header*>(v - pad_before);
     void* n = malloc(size);
     memcpy(n, v, h->size);
     free(v);
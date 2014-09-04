loudinthecloud/osv

commit a4b52857d38427c784adeee48a3a513f89fd3450

build: fix tst-static-thread-variable.so dependencies (again)
Commit 7a69d0d8 and 99d80368 introduced:

  tests/tst-static-thread-variable.so: COMMON += -L./tests -lstatic-thread-variable

However make variables are inherited by prerequisites, which will cause
libstatic-thread-variable.so being added as its dependencies itself,
resulting in this error:

  ./tests/libstatic-thread-variable.so: file not recognized: File truncated

Add 'private' keyword [1] to surpass the inheritance.

[1]: https://www.gnu.org/software/make/manual/html_node/Suppressing-Inheritance.html

Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
Signed-off-by: Zifei Tong <zifeitong@gmail.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

build.mk
@@ -320,7 +320,7 @@ tests += tests/tst-hostname.so
 tests += tests/tst-sendfile.so
 tests += tests/libstatic-thread-variable.so tests/tst-static-thread-variable.so
 tests/tst-static-thread-variable.so: tests/libstatic-thread-variable.so
-tests/tst-static-thread-variable.so: COMMON += -L./tests -lstatic-thread-variable
+tests/tst-static-thread-variable.so: private COMMON += -L./tests -lstatic-thread-variable
 tests += tests/misc-lock-perf.so
 tests += tests/tst-uio.so
 tests += tests/tst-printf.so

commit 87cab38b1c1a35ebd0f088d73de0b0dd5a104379

build: do not use 'private' in build.mk
Commit a4b52857d38427c784adeee48a3a513f89fd3450 introduced 'private'
keyword to solve a dependencies issue caused by the inheritance of
target-specific variable. The keyword however is only available since
make 3.82.

This patch removes the '-lstatic-thread-variable' option. Since we've
already added the library as one of the target's prerequisites and
compiling the target with '$^', that option is not needed.

Fixes #405.

Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
Signed-off-by: Zifei Tong <zifeitong@gmail.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

build.mk
@@ -345,7 +345,6 @@ tests += tests/tst-hostname.so
 tests += tests/tst-sendfile.so
 tests += tests/libstatic-thread-variable.so tests/tst-static-thread-variable.so
 tests/tst-static-thread-variable.so: tests/libstatic-thread-variable.so
-tests/tst-static-thread-variable.so: private COMMON += -L./tests -lstatic-thread-variable
 tests += tests/misc-lock-perf.so
 tests += tests/tst-uio.so
 tests += tests/tst-printf.so
@@ -355,12 +354,6 @@ tests += tests/tst-thread-local.so
 tests += tests/tst-app.so
 tests += tests/misc-gtod.so
 tests += tests/misc-concurrent-io.so
-endif
-
-ifeq ($(arch),aarch64)
-lib_tests :=
-else
-lib_tests := tests/libstatic-thread-variable.so
 endif
 
 tests/hello/Hello.class: javabase=tests/hello
@@ -1051,8 +1044,6 @@ $(src)/modules/tests/usr.manifest: $(src)/build.mk
 	@echo "  generating modules/tests/usr.manifest"
 	@cat $@.skel > $@
 	@echo $(tests) | tr ' ' '\n' | awk '{print "/" $$0 ": ./" $$0}' >> $@
-	@echo $(lib_tests) | tr ' ' '\n' | \
-		awk '{name=$$0; name=gensub(".*/([^/]*)", "\\1", name); print "/usr/lib/" name ": ./" $$0}' >> $@
 	@echo $(java_tests) | tr ' ' '\n' | \
 	    awk '{a=$$0; sub(".*/","",a); print "/java/" a ": ./" $$0}' >> $@
 